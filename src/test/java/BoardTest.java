29b40c3b2dfe6989beea33d95bfb253e8c3863e2
alexchimzi <chilaeze@gmail.com>
2022-05-18 14:08:51 +0100

    added test file. simple test

---
+++ src/test/java/BoardTest.java
@@ -0,0 +1,12 @@
+import org.example.Board;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class BoardTest {
+    Board b = new Board();
+    @Test
+    public void testInvalidInput(){
+        Assertions.assertTrue(true);
+
+    }
+}
